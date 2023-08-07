f32970b26205812ca46b896956d10b1725240144
andy30696 <andy30696@hotmail.com>
2023-08-07 10:17:46 -0300

    Intento

--- evento.js
+++ evento.js
@@ -1,3 +1,12 @@
 function saludar(){
     alert("Hola!")
 }
+
+document.addEventListener("DOMContentLoaded", function (){
+    let boton = document.getElementsByTagName("div")
+    boton.addEventListener("click", function(){
+        alert("Hola! Soy el div")
+    }
+    )
+} 
+)
